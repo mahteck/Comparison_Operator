7ea9c50a186f7c234b55260b721c3818fbd09b1f
mshoaib1244 <shoaib1244@gmail.com>
2024-04-29 19:47:31 +0500

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let a = 5;
+let b = 2;
+let isEqual = (a == b); //false
+console.log(isEqual);
+let isNotEqual = (a != b); //true
+console.log(isNotEqual);
+let isGreaterThan = (a > b); //true
+console.log(isGreaterThan);
+let isLessthan = (a < b); //false
+console.log(isLessthan);
---
+++ main.ts
@@ -0,0 +1,12 @@
+let a: number = 5;
+let b: number = 2;
+
+let isEqual = (a == b); //false
+console.log(isEqual);
+let isNotEqual = (a != b) //true
+console.log(isNotEqual)
+let isGreaterThan = (a > b); //true
+console.log(isGreaterThan);
+let isLessthan = (a < b); //false
+console.log(isLessthan);
+
